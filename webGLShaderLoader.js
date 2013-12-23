10d650facce32ad1f85fa3399d2d2096777c8edf
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-12-23 04:26:22 -0500

    better error handling

--- webGLShaderLoader.js
+++ webGLShaderLoader.js
@@ -8,16 +8,17 @@ var WebGLShaderLoader = (function () {
     this.errors = [];
   };
 
-  // todo: console.error -> this.errors.push(""); cb(errors, null);
   WebGLShaderLoader.prototype = {
     loadFromStr: function (vertexShaderStr, fragmentShaderStr, cb) {
       if (typeof vertexShaderStr !== "string" ||
           typeof fragmentShaderStr !== "string" ||
           typeof cb !== "function") {
-        console.error("Usage: loaderInstance.loadFromStr('', '', function (errors, program));");
+        this.errors.push("Usage: loaderInstance.loadFromStr('', '', function (errors, program));");
+        cb(this.errors, null);
       } else if (vertexShaderStr.length === 0 ||
                  fragmentShaderStr.length === 0) {
-        console.error("empty shader string");
+        this.errors.push("empty shader string");
+        cb(this.errors, null);
       } else {
         this.vertexShader = this.compile(vertexShaderType, vertexShaderStr);
         this.fragmentShader = this.compile(fragmentShaderType, fragmentShaderStr);
@@ -29,19 +30,27 @@ var WebGLShaderLoader = (function () {
       if (typeof vertexShaderPath !== "string" ||
           typeof fragmentShaderPath !== "string" ||
           typeof cb !== "function") {
-        console.error("Usage: loaderInstance.loadFromXHR('', '', function (errors, program));");
+        this.errors.push("Usage: loaderInstance.loadFromXHR('', '', function (errors, program));");
+        cb(this.errors, null);
         return;
       }
 
       if (vertexShaderPath.length === 0 || fragmentShaderPath.length === 0) {
-        console.error("empty shader path");
+        this.errors.push("empty shader path");
+        cb(this.errors, null);
         return;
       }
 
       var numShadersLoaded = 0;
       var self = this;
       var onload = function (shader, shaderType) {
-        return function (shaderStr) {
+        return function (twoHundredResponse, shaderStr) {
+          if (!twoHundredResponse) {
+            self.errors.push("xhr non 200 response code");
+            cb(self.errors, null);
+            return;
+          }
+
           self[shader] = self.compile(shaderType, shaderStr);
 
           if (++numShadersLoaded > 1) {
@@ -79,9 +88,7 @@ var WebGLShaderLoader = (function () {
       var xhr = new XMLHttpRequest;
       xhr.open("GET", path);
       xhr.onload = function () {
-        // todo:
-        // check for non 200
-        cb(xhr.response);
+        cb(xhr.status === 200, xhr.response);
       };
       xhr.send();
     },
