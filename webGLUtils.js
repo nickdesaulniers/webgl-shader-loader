33be2a3ad7130892620f5fe96822bdfcae28ddeb
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-12-28 13:38:33 -0500

    added init buffer

--- webGLUtils.js
+++ webGLUtils.js
@@ -14,8 +14,19 @@
     };
   };
 
+  // num is number of components per attribute.  ex. vec3 -> 3, mat4 -> 4
+  function initBuffer (gl, data, num, attribute) {
+    var buffer = gl.createBuffer();
+    if (!buffer) return !!console.error("Failed to create " + attribute + " buffer.");
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(attribute, num, gl.FLOAT, false, 0, 0);
+    return !gl.enableVertexAttribArray(attribute);
+  };
+
   var webGLProto = WebGLRenderingContext.prototype;
   global.getAttributes = getQualifiersPartial(webGLProto.ACTIVE_ATTRIBUTES, "Attrib");
   global.getUniforms = getQualifiersPartial(webGLProto.ACTIVE_UNIFORMS, "Uniform");
+  global.initBuffer = initBuffer;
 })(this);
 
